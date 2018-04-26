1726ea887047476a0e28b45db1a6e664455df556
Matt McFarland <783024+vanetix@users.noreply.github.com>
2018-04-26 14:32:42 -0500

    Update replicateZone logic with initial working functionality

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -23,17 +24,23 @@ func init() {
 	flag.StringVar(&target, "t", "", "target of domain name")
 	flag.StringVar(&zoneId, "z", "", "AWS Zone Id for domain")
 	//flag.Int64Var(&TTL, "ttl", int64(60), "TTL for DNS Cache")
-
-	fmt.Println(name)
-	fmt.Println(target)
-	fmt.Println(zoneId)
-	fmt.Println(TTL)
-	fmt.Println(weight)
 }
 
 func main() {
+	// Establish AWS session
+	sess, err := session.NewSession()
+	if err != nil {
+		fmt.Println("failed to create session,", err)
+		return
+	}
+
+	svc := route53.New(sess)
+
+	//  HERP DERP DERP DERP DREP .
+	flag.Parse()
+
 	//Active Directory zones being pulled
-	zones := []string{"in.creditcards.com.", "staging.in.creditcards.com."}
+	zones := []string{"in.creditcards.com."}
 
 	for _, zone := range zones {
 		// ccads.in.creditcars.com - 10.11.100.30
@@ -43,7 +50,9 @@ func main() {
 			log.Fatalf("Error fetching records: %s\n", err)
 		}
 
-		if err := replicateRecords(records); err != nil {
+		fmt.Printf("Total record count: %s\n", len(records))
+
+		if err := replicateRecords(svc, records); err != nil {
 			log.Printf("Error replicating zone %s: %s\n", zone, err)
 		}
 	}
@@ -51,22 +60,13 @@ func main() {
 
 	// Start Route53
 
-	flag.Parse()
 	if name == "" || target == "" || zoneId == "" {
 		fmt.Println(fmt.Errorf("Incomplete arguments: d: %s, t: %s, z: %s\n", name, target, zoneId))
 		flag.PrintDefaults()
 		return
 	}
 
-	// Establish AWS session
-	sess, err := session.NewSession()
-	if err != nil {
-		fmt.Println("failed to create session,", err)
-		return
-	}
-	svc := route53.New(sess)
-
-	listCNAMES(svc)
+	//listCNAMES(svc)
 
 }
 
@@ -97,20 +97,99 @@ func transferRecords(z string, ns string) ([]dns.RR, error) {
 	return records, nil
 }
 
-func replicateRecords(rs []dns.RR) error {
+// TODO: This requires quite a bit of cleanup
+func replicateRecords(svc *route53.Route53, rs []dns.RR) error {
+	cs := &route53.ChangeResourceRecordSetsInput{
+		HostedZoneId: aws.String(zoneId),
+	}
+	cb := &route53.ChangeBatch{
+		Comment: aws.String(time.Now().UTC().String()),
+	}
+	var changes []*route53.Change
+
 	for _, record := range rs {
+		rrs := &route53.ResourceRecordSet{
+			TTL:  aws.Int64(60),
+			Name: aws.String(record.Header().Name),
+		}
+
 		switch t := interface{}(record).(type) {
 		case *dns.A:
-			fmt.Printf("A: \"%v\", \"%v\"\n", t.Hdr.Name, t.A)
+			rrs.SetType("A")
+			rrs.ResourceRecords = []*route53.ResourceRecord{
+				{
+					Value: aws.String(t.A.String()),
+				},
+			}
 		case *dns.CNAME:
-			fmt.Printf("CNAME: \"%v\", \"%v\"\n", t.Hdr.Name, t.Target)
+			rrs.SetType("CNAME")
+			rrs.ResourceRecords = []*route53.ResourceRecord{
+				{
+					Value: aws.String(t.Target),
+				},
+			}
 		case *dns.MX:
-			fmt.Printf("MX: \"%v\", \"%v\"\n", t.Hdr.Name, t.Mx)
+			rrs.SetType("MX")
+			rrs.ResourceRecords = []*route53.ResourceRecord{
+				{
+					Value: aws.String(t.Mx),
+				},
+			}
 		case *dns.TXT:
-			fmt.Printf("TXT: \"%v\", \"%v\"\n", t.Hdr.Name, t.Txt)
+			value := fmt.Sprintf("\"%s\"", strings.Join(t.Txt, " "))
+
+			rrs.SetType("TXT")
+			rrs.ResourceRecords = []*route53.ResourceRecord{
+				{
+					Value: aws.String(value),
+				},
+			}
 		default:
 			// NOOP
+			continue
 		}
+
+		c := &route53.Change{
+			Action:            aws.String("UPSERT"),
+			ResourceRecordSet: rrs,
+		}
+
+		// Print out any validation errors
+		if err := rrs.Validate(); err != nil {
+			fmt.Printf("Invalid record: %s, %s", record.Header().Name, err)
+		} else {
+			if l := len(changes); l > 0 && *changes[l-1].ResourceRecordSet.Name == record.Header().Name {
+				previous := changes[l-1].ResourceRecordSet.ResourceRecords
+
+				previous = append(previous, rrs.ResourceRecords...)
+			} else {
+				changes = append(changes, c)
+			}
+		}
+
+	}
+
+	// TODO: Turn this into a loop on 500 record chunks
+	chunkSize := 500
+	for chunk := 0; (chunk * chunkSize) < len(changes); chunk++ {
+		var bounds int
+
+		if next := chunk*chunkSize + chunkSize; next < len(changes) {
+			bounds = next
+		} else {
+			bounds = len(changes)
+		}
+
+		cb.SetChanges(changes[chunk*chunkSize : bounds])
+		cs.SetChangeBatch(cb)
+
+		resp, err := svc.ChangeResourceRecordSets(cs)
+
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("%#v\n", resp)
 	}
 
 	return nil
@@ -125,11 +204,12 @@ func listCNAMES(svc *route53.Route53) {
 		// Test static zone below
 		//HostedZoneId: aws.String("Z3BWSUB0RPS89Q"), // Required
 
-		// MaxItems:              aws.String("100"),
+		MaxItems: aws.String("1000"),
 		// StartRecordIdentifier: aws.String("Sample update."),
 		// StartRecordName:       aws.String("com."),
 		// StartRecordType:       aws.String("CNAME"),
 	}
+
 	respList, err := svc.ListResourceRecordSets(listParams)
 
 	if err != nil {
